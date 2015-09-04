671092655b1bebcd189dee503e83fa2a824e737c
Felix Trepanier <felix.trepanier@opower.com>
2015-09-04 14:48:02 -0400

    Accept application/json without charset

--- src/main/scala/com/twitter/diffy/lifter/HttpLifter.scala
+++ src/main/scala/com/twitter/diffy/lifter/HttpLifter.scala
@@ -63,7 +63,7 @@ class HttpLifter(excludeHttpHeadersComparison: Boolean) {
           )
 
         /** When Content-Type is set as application/json, lift as Json **/
-        case (Some(mediaType), _) if mediaType.is(MediaType.JSON_UTF_8) => {
+        case (Some(mediaType), _) if mediaType.is(MediaType.JSON_UTF_8) || mediaType.toString == "application/json" => {
           val jsonContentTry = Try {
             JsonLifter.decode(r.getContent.toString(Charsets.Utf8))
           }
